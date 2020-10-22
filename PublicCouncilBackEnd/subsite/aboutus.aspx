cfd019fc38d8e86964950a5b6ed65c947693f793
Bahram Bayramli <bahramnb@code.edu.az>
2020-10-22 16:17:11 +0400

    added query code to about us

--- PublicCouncilBackEnd/subsite/aboutus.aspx
+++ PublicCouncilBackEnd/subsite/aboutus.aspx
@@ -1,5 +1,26 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/subsite/Subdomain.Master" AutoEventWireup="true" CodeBehind="aboutus.aspx.cs" Inherits="PublicCouncilBackEnd.subsite.WebForm9" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+<%--    <link href="../content/css/argon-design-system.min.css" rel="stylesheet" />--%>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="sublayout" runat="server">
+     <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <asp:HyperLink 
+                        ID="pageName" 
+                        runat="server" 
+                        CssClass="d-block text-white p-2 mb-2 bg-default rounded shadow-sm post-block-title text-center text-uppercase" 
+                        Style="font-size: 2rem">
+                    </asp:HyperLink>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="about-us">
+                        <asp:Literal ID="aboususInfo" runat="server"></asp:Literal>
+                    </div>
+                </div>
+            </div>
+            
+     </div>
 </asp:Content>
--- PublicCouncilBackEnd/subsite/aboutus.aspx.cs
+++ PublicCouncilBackEnd/subsite/aboutus.aspx.cs
@@ -4,14 +4,54 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace PublicCouncilBackEnd.subsite
 {
     public partial class WebForm9 : System.Web.UI.Page
     {
+
+        private void GetUserInfo(string USER_PCDOMAIN)
+        {
+            SqlDataAdapter getSerial = new SqlDataAdapter(new SqlCommand(@"SELECTPC_ABOUT  FROM  PC_USERS 
+                                                                           WHERE 
+                                                                                 ISDELETE      = @ISDELETE AND 
+                                                                                 ISACTIVE      = @ISACTIVE AND
+                                                                                 USER_PCDOMAIN = @USER_PCDOMAIN"));
+            getSerial.SelectCommand.Parameters.Add("@ISDELETE", SqlDbType.Bit).Value = false;
+            getSerial.SelectCommand.Parameters.Add("@ISACTIVE", SqlDbType.Bit).Value = true;
+            getSerial.SelectCommand.Parameters.Add("@USER_PCDOMAIN", SqlDbType.NVarChar).Value = USER_PCDOMAIN;
+
+            DataTable dt = SQL.SELECT(getSerial);
+
+            aboususInfo.Text = dt.Rows[0]["PC_ABOUT"].ToString();
+            dt = null;
+
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            switch (Convert.ToString(Page.RouteData.Values["language"]).ToLower())
+            {
+                case "az":
+                    {
+                        pageName.Text = "Haqqımızda";
+                        break;
+                    }
+                case "en":
+                    {
+                        pageName.Text = "About Us";
+                        break;
+                    }
+                default:
+                    {
+                        pageName.Text = "Haqqımızda";
+                        break;
+                    }
+            }
 
+            GetUserInfo(Session["pcsubsite"] as string);
         }
     }
 }
