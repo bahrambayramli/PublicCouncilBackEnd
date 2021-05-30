25b8f3798e7b47ba766523580f2171d27c1376e2
Bahram Bayramli <bahramnb@code.edu.az>
2021-05-30 22:30:56 +0400

    update

--- PublicCouncilBackEnd/subsite/memberdetail.aspx.cs
+++ PublicCouncilBackEnd/subsite/memberdetail.aspx.cs
@@ -1,17 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace PublicCouncilBackEnd.subsite
 {
     public partial class WebForm11 : System.Web.UI.Page
     {
+        #region(SQL FUNCTIONS)
 
+        private string GetPcId(string PC_NAME)
+        {
+            SqlDataAdapter getPcId = new SqlDataAdapter(new SqlCommand(@"SELECT USER_ID FROM PC_USERS WHERE USER_PCDOMAIN = @USER_PCDOMAIN"));
+
+            getPcId.SelectCommand.Parameters.Add("@USER_PCDOMAIN", SqlDbType.NVarChar).Value = PC_NAME;
+
+            return SQL.SELECT(getPcId).Rows[0]["USER_ID"].ToString();
+
+        }
         private void GetMemberInfo(string LANG, string MEMBER_ID, string PC_ID)
         {
             SqlDataAdapter getMember;
@@ -38,10 +45,10 @@ namespace PublicCouncilBackEnd.subsite
                         getMember.SelectCommand.Parameters.Add("@MEMBER_ID", SqlDbType.Int).Value = MEMBER_ID;
                         dt = SQL.SELECT(getMember);
 
-                        memberImage.ImageUrl   = $"~/images/members/{dt.Rows[0]["MEMBER_IMAGE"].ToString()}";
-                        memberPosition.Text    = $"{dt.Rows[0]["MEMBER_POSITION_AZ"].ToString()}";
+                        memberImage.ImageUrl = $"~/images/members/{dt.Rows[0]["MEMBER_IMAGE"].ToString()}";
+                        memberPosition.Text = $"{dt.Rows[0]["MEMBER_POSITION_AZ"].ToString()}";
                         memberNameSurname.Text = $"{dt.Rows[0]["MEMBER_NAME_AZ"].ToString()} {dt.Rows[0]["MEMBER_SURNAME_AZ"].ToString()}";
-                        memberDetail.Text      = $"{dt.Rows[0]["MEMBER_DETAIL_AZ"].ToString()}";
+                        memberDetail.Text = $"{dt.Rows[0]["MEMBER_DETAIL_AZ"].ToString()}";
                         break;
                     }
                 case "en":
@@ -101,9 +108,31 @@ namespace PublicCouncilBackEnd.subsite
             getMember = null;
             dt = null;
         }
+        #endregion
+
+        protected private void RunMemberDetail(string LANG, string PC_NAME, string MEMBER_ID)
+        {
+            try
+            {
+                GetMemberInfo(LANG, MEMBER_ID, GetPcId(PC_NAME));
+            }
+            catch (Exception ex)
+            {
+                Log.LogCreator(Server.MapPath(Path.Combine("~/Logs", "log.txt")), $"Log created:{DateTime.Now}, Log page is: subsute >> memberdetail.aspx page >> GetMemberInfo, Log:{ex.Message}");
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            GetMemberInfo(Convert.ToString(Page.RouteData.Values["language"]).ToLower(), Page.RouteData.Values["memberid"] as string, Session["PC_USER_ID"] as string);
+            try
+            {
+                RunMemberDetail(Convert.ToString(Page.RouteData.Values["language"]).ToLower(), Convert.ToString(Page.RouteData.Values["publiccouncil"]).ToLower(), Page.RouteData.Values["memberid"] as string);
+            }
+            catch (Exception ex)
+            {
+                Log.LogCreator(Server.MapPath(Path.Combine("~/Logs", "log.txt")), $"Log created:{DateTime.Now}, Log page is: subsute memberdetail.aspx page >> RunMemberDetail, Log:{ex.Message}");
+            }
+
         }
     }
 }
