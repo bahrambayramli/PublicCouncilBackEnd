c64cf7a4ae2c1bf32ad04fb7b68285fa611f3110
Bahram Bayramli <bahramnb@code.edu.az>
2021-05-31 20:18:23 +0400

    update subsite >> aboutus.aspx file

--- PublicCouncilBackEnd/subsite/aboutus.aspx.cs
+++ PublicCouncilBackEnd/subsite/aboutus.aspx.cs
@@ -1,78 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace PublicCouncilBackEnd.subsite
 {
     public partial class WebForm9 : System.Web.UI.Page
     {
-
+        #region(SQL FUNCTIONS)
         private void GetUserInfo(string LANG, string USER_PCDOMAIN)
         {
             SqlDataAdapter getSerial;
-            DataTable dt;
+            DataTable DT;
             switch (LANG)
             {
                 case "az":
                     {
-                        getSerial = new SqlDataAdapter(new SqlCommand(@"SELECT PC_NAME, PC_ABOUT_AZ  FROM  PC_USERS 
-                                                                           WHERE 
+                        getSerial = new SqlDataAdapter(new SqlCommand(@"SELECT  
+                                                                                PC_NAME, 
+                                                                                PC_ABOUT_AZ AS PC_ABOUT
+
+                                                                        FROM    PC_USERS 
+
+                                                                        WHERE 
                                                                                  ISDELETE      = @ISDELETE AND 
                                                                                  ISACTIVE      = @ISACTIVE AND
                                                                                  USER_PCDOMAIN = @USER_PCDOMAIN"));
-                        getSerial.SelectCommand.Parameters.Add("@ISDELETE", SqlDbType.Bit).Value = false;
-                        getSerial.SelectCommand.Parameters.Add("@ISACTIVE", SqlDbType.Bit).Value = true;
-                        getSerial.SelectCommand.Parameters.Add("@USER_PCDOMAIN", SqlDbType.NVarChar).Value = USER_PCDOMAIN;
-
-                        dt = SQL.SELECT(getSerial);
-                        aboususInfo.Text = dt.Rows[0]["PC_ABOUT_AZ"].ToString();
                         break;
                     }
                 case "en":
                     {
-                        getSerial = new SqlDataAdapter(new SqlCommand(@"SELECT PC_NAME, PC_ABOUT_EN  FROM  PC_USERS 
-                                                                           WHERE 
+                        getSerial = new SqlDataAdapter(new SqlCommand(@"SELECT  
+                                                                                PC_NAME, 
+                                                                                PC_ABOUT_EN AS PC_ABOUT
+
+                                                                        FROM    PC_USERS 
+
+                                                                        WHERE 
                                                                                  ISDELETE      = @ISDELETE AND 
                                                                                  ISACTIVE      = @ISACTIVE AND
                                                                                  USER_PCDOMAIN = @USER_PCDOMAIN"));
-                        getSerial.SelectCommand.Parameters.Add("@ISDELETE", SqlDbType.Bit).Value = false;
-                        getSerial.SelectCommand.Parameters.Add("@ISACTIVE", SqlDbType.Bit).Value = true;
-                        getSerial.SelectCommand.Parameters.Add("@USER_PCDOMAIN", SqlDbType.NVarChar).Value = USER_PCDOMAIN;
-
-                        dt = SQL.SELECT(getSerial);
-                        aboususInfo.Text = dt.Rows[0]["PC_ABOUT_EN"].ToString();
                         break;
                     }
                 default:
                     {
-                        getSerial = new SqlDataAdapter(new SqlCommand(@"SELECT PC_NAME, PC_ABOUT_AZ  FROM  PC_USERS 
-                                                                           WHERE 
+                        getSerial = new SqlDataAdapter(new SqlCommand(@"SELECT  
+                                                                                PC_NAME, 
+                                                                                PC_ABOUT_AZ AS PC_ABOUT
+
+                                                                        FROM    PC_USERS 
+
+                                                                        WHERE 
                                                                                  ISDELETE      = @ISDELETE AND 
                                                                                  ISACTIVE      = @ISACTIVE AND
                                                                                  USER_PCDOMAIN = @USER_PCDOMAIN"));
-                        getSerial.SelectCommand.Parameters.Add("@ISDELETE", SqlDbType.Bit).Value = false;
-                        getSerial.SelectCommand.Parameters.Add("@ISACTIVE", SqlDbType.Bit).Value = true;
-                        getSerial.SelectCommand.Parameters.Add("@USER_PCDOMAIN", SqlDbType.NVarChar).Value = USER_PCDOMAIN;
-
-                        dt = SQL.SELECT(getSerial);
-                        aboususInfo.Text = dt.Rows[0]["PC_ABOUT_AZ"].ToString();
                         break;
                     }
             }
 
-            dt = null;
+            getSerial.SelectCommand.Parameters.Add("@ISDELETE", SqlDbType.Bit).Value                = false;
+            getSerial.SelectCommand.Parameters.Add("@ISACTIVE", SqlDbType.Bit).Value                = true;
+            getSerial.SelectCommand.Parameters.Add("@USER_PCDOMAIN", SqlDbType.NVarChar).Value      = USER_PCDOMAIN;
+
+            DT = SQL.SELECT(getSerial);
+            aboususInfo.Text = DT.Rows[0]["PC_ABOUT"].ToString();
+
+        }
+        #endregion
+
+        protected private void RunAboutUs(string LANG, string PC_NAME)
+        {
+            try
+            {
+                GetUserInfo(LANG, PC_NAME);
+            }
+            catch (Exception ex)
+            {
+                Log.LogCreator(Server.MapPath(Path.Combine("~/Logs", "log.txt")), $"Log created:{DateTime.Now}, Log page is: subsite >>aboutus.aspx page >> GetUserInfo method, Log:{ex.Message}");
+            }
         }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-     
+            try
+            {
+                RunAboutUs(Convert.ToString(Page.RouteData.Values["language"]).ToLower(), Page.RouteData.Values["publiccouncil"] as string);
+            }
+            catch (Exception ex)
+            {
+                Log.LogCreator(Server.MapPath(Path.Combine("~/Logs", "log.txt")), $"Log created:{DateTime.Now}, Log page is: subsite >>aboutus.aspx page >> RunAboutUs method, Log:{ex.Message}");
+            }
 
-            GetUserInfo(Convert.ToString(Page.RouteData.Values["language"]).ToLower(), Page.RouteData.Values["publiccouncil"] as string);
         }
+
     }
 }
